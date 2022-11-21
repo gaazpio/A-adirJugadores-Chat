962941af2c95834c149abbb3a5568f19ce79cef3
David <davidgazpio40@gmail.com>
2022-11-21 12:50:07 +0100

    creamos funcion para logearse

--- lib/src/login_views/LoginView.dart
+++ lib/src/login_views/LoginView.dart
@@ -1,12 +1,30 @@
 //CREAMOS LA CLASE LOGIN PARA LOGEARTE EN LA APLICACION//
 
 import 'package:flutter/material.dart';
-
+import 'package:firebase_auth/firebase_auth.dart';
 import '../custom_views/InputTexts.dart';
 
 class Login extends StatelessWidget{
   Login({Key? key}) : super(key: key);
 
+
+  void logearse(String emailAddress, String password, BuildContext context) async{
+
+    try {
+      final credential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: emailAddress,
+        password: password,
+      );
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        print('The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        print('The account already exists for that email.');
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
