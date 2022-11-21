149fee1a58bc67abfb57f236ecb498c757b06938
David <davidgazpio40@gmail.com>
2022-11-21 11:59:17 +0100

    configuracion de main y creacion de app para correr las clases creadas

---
+++ lib/App.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+
+
+}
