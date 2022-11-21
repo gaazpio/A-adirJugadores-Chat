4e4cc37d6979415327ac37dc9a04bf3c7fa6c54b
David <davidgazpio40@gmail.com>
2022-11-21 11:53:47 +0100

    creacion clase login

---
+++ lib/login_views/LoginView.dart
@@ -0,0 +1,19 @@
+//CREAMOS LA CLASE LOGIN PARA LOGEARTE EN LA APLICACION//
+
+import 'package:flutter/material.dart';
+
+class Login extends StatelessWidget{
+  Login({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+
+    return Scaffold(
+    );
+
+  }
+
+
+
+
+}
