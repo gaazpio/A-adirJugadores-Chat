be1eed4a3f962b163e6eb32b0284093d71d9f996
David <davidgazpio40@gmail.com>
2022-11-22 12:39:26 +0100

    clase list

--- lib/src/custom_views/ListItem.dart
+++ lib/src/custom_views/ListItem.dart
@@ -1,11 +1,23 @@
 import 'package:flutter/material.dart';
 
 class ListItem extends StatelessWidget{
+  final String stittle;
+  final int iIndex;
+  final Function(int index) whitClickGo;
+const ListItem({Key? key , required this.stittle, required this.whitClickGo, required this.iIndex,}) : super(key: key);
   @override
+
+
   Widget build(BuildContext context) {
     // TODO: implement build
-    throw UnimplementedError();
+    return ListTile(
+      title: Text(stittle, textScaleFactor: 1.1,),
+      leading: Icon(Icons.person),
+      onTap: (){
+        whitClickGo(iIndex);
+      },
+    );
   }
-  
-  
+
+
 }
