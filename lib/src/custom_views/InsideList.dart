e989e708eb2c69789cd97f33cbd8980ae13294b3
David <davidgazpio40@gmail.com>
2022-11-23 12:00:12 +0100

    Scaffold de Insidelist

--- lib/src/custom_views/InsideList.dart
+++ lib/src/custom_views/InsideList.dart
@@ -20,7 +20,28 @@ class _InsideList extends State<InsideList> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(
+        leading: Icon(Icons.filter_none),
+        title: Text("hola"),
+        backgroundColor: Color.fromRGBO(112, 0, 0, 20),
+      ),
+      body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                height: 685,
+              ),
+              Padding(padding: EdgeInsets.fromLTRB(0, 0, 0, 0), child:Row (
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Padding(padding: EdgeInsets.fromLTRB(0, 7, 0, 0), child: Text("HOLA")),
+                ],
+              ),
+              ),
+            ],
+          )),
+    );
 
 
   }
