44981dcead5295d3aec466ace6ed66b560d96397
David <davidgazpio40@gmail.com>
2022-11-23 11:53:22 +0100

    declaracion clase statefulwidget insidelist

---
+++ lib/src/custom_views/InsideList.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+
+class InsideList extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _InsideList();
+  }
+}
+
+class _InsideList extends State<InsideList> {
+  @override
+
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+
+
+  }
+}
